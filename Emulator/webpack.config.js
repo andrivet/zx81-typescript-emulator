5eaf531d775999aa5e7796481be3d86ce5404738
Sebastien Andrivet <sebastien@advtools.com>
2017-07-21 18:23:18 +0200

    Use Webpack CommonsChunckPlugin and HtmpwebpackPlugin

--- Emulator/webpack.config.js
+++ Emulator/webpack.config.js
@@ -1,19 +1,16 @@
 
 const path = require('path');
 const webpack = require('webpack'); //to access built-in plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        app: [
-            'es6-promise',
-            'whatwg-fetch',
-            "./js/zx81emulator/ZX81Emulator.ts"
-
-        ]
+        polyfill: "./js/polyfill/polyfill.ts",
+        app: "./js/zx81emulator/ZX81Emulator.ts"
     },
     output: {
         path: path.resolve(__dirname, "build"),
-        filename: "bundle.js"
+        filename: "[name].js"
     },
     watch: false,
     devtool: "source-map",
@@ -26,7 +23,19 @@ module.exports = {
         ]
     },
     plugins: [
-        new webpack.ProvidePlugin({'Promise': 'es6-promise'})
+        new webpack.optimize.CommonsChunkPlugin({
+            names: [
+                "main",
+                "polyfill",
+                "bootstrap" // Extract the Webpack bootstrap logic into its own file by providing a name that wasn't listed in the "entry" file list.
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            title: "ZX81 Emulator",
+            template: './js/index.ejs',
+            inject: true,
+            filename: '../index.html'
+        })
     ]
 };
 

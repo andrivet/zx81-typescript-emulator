47f8f21925807e08a325ccad4b7513a6217b102f
Sebastien Andrivet <sebastien@advtools.com>
2017-07-31 16:56:09 +0200

    Set debug and sourceMap globally with LoaderOptionsPlugin

--- Emulator/webpack.dev.js
+++ Emulator/webpack.dev.js
@@ -1,9 +1,14 @@
 const Merge = require("webpack-merge");
+const webpack = require('webpack'); //to access built-in plugins
 const CommonConfig = require("./webpack.common.js");
 
 module.exports = Merge(CommonConfig, {
     watch: true,
     devtool: "cheap-module-eval-source-map",
     plugins: [
+        new webpack.LoaderOptionsPlugin({
+            debug: true,
+            sourceMap: true
+        })
     ]
 });

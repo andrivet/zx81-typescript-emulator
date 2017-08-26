0dd9537c0240695d8b8f061fe1ca8f01f5175d22
Sebastien Andrivet <sebastien@advtools.com>
2017-08-26 23:04:39 +0200

    Refine the public API (d.ts)

--- zx81-typescript-emulator/src/index.d.ts
+++ zx81-typescript-emulator/src/index.d.ts
@@ -1,27 +1,30 @@
-// Type definitions for zx81-typescript-emulator
+// Type definitions for zx81-emulator
 // Project: https://github.com/andrivet/zx81-typescript-emulator
 // Definitions by: Sebastien Andrivet <http://github.com/andrivet>
 
-declare class ZX81Emulator
+declare module "zx81-emulator"
 {
-    constructor(canvas: HTMLCanvasElement, status: ZX81Emulator.Status);
-
-    load(fileName: string, rom: string, scale: number): Promise<void>;
-    setStatus(kind: ZX81Emulator.StatusKind, message: string): void;
-    start(rom: string): Promise<void>;
-    stop(): void;
-    onKeyDown(event: KeyboardEvent): any;
-    onKeyUp(event: KeyboardEvent): any;
-}
-
-declare namespace ZX81Emulator
-{
-    export const enum StatusKind { OK, Info, Warning, Error }
-
-    export interface Status
+    namespace ZX81Emulator
     {
-        status(message: string, kind: StatusKind, previousKind: StatusKind): void;
-    }
-}
+        const enum StatusKind { OK, Info, Warning, Error }
 
-export default ZX81Emulator;
+        interface Status
+        {
+            status(message: string, kind: StatusKind, previousKind: StatusKind): void;
+        }
+    }
+
+    class ZX81Emulator
+    {
+        constructor(canvas: HTMLCanvasElement, status: ZX81Emulator.Status);
+
+        load(fileName: string, rom: string, scale: number): Promise<void>;
+        setStatus(kind: ZX81Emulator.StatusKind, message: string): void;
+        start(rom: string): Promise<void>;
+        stop(): void;
+        onKeyDown(event: KeyboardEvent): any;
+        onKeyUp(event: KeyboardEvent): any;
+    }
+
+    export default ZX81Emulator;
+}
